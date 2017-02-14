96690e5e44a2431e61997cdb57c523d8eca23f29
Farid Hajnal <faridhajnal14@hotmail.com>
2017-02-14 14:48:46 -0600

    moving fb secret to secure spot

--- config/facebook.js
+++ config/facebook.js
@@ -1,7 +1,8 @@
 // facebook app settings - fb.js
+const fbsecret = require('./facebook-secret');
 module.exports = {
    appID : '287041178379781',
-   appSecret : '0b69a7a917f9e7f455ea7354da602ba0',
+   appSecret : fbsecret,
    callbackUrl : 'http://localhost:3000/user/login/facebook/callback',
    profileFields: ['id', , 'email', 'displayName', 'name']
 }
